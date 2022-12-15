38e616c27013f437940b4cfd64baf8018f9fa184
Sara Song <shsong@andrew.cmu.edu>
2022-12-14 23:12:32 -0500

    set root in routes to children index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # Generated routes for models
   resources :chores
   resources :tasks
   resources :children
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  # Setting default route
+  root to: 'chores#index'
+  
 end
